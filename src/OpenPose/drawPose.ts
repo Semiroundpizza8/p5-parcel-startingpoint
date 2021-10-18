f92eaa2f77aa267f39bee4608fed2bd5bdebcb13
Benjamin Odisho <Semiroundpizza8@gmail.com>
2021-10-17 20:55:57 -0500

    Updated part-map

--- src/OpenPose/drawPose.ts
+++ src/OpenPose/drawPose.ts
@@ -1,6 +1,8 @@
 export const drawPose = (p, frame) => {
   for (var i = 0; i < frame.parts.length; i++) {
     const part = frame.parts[i];
-    p.circle(part.x, part.y, 15);
+    // p.circle(part.x, part.y, 15);
+    p.fill(255);
+    p.text(part.part, part.x, part.y);
   }
 };
